75ae235de65bcc920570ff0659110263cb662291
jzscsaba <johanyak.csaba@nje.hu>
2025-02-19 14:48:35 +0100

    Add project files.

---
+++ valtozo.cpp
@@ -0,0 +1,15 @@
+/****************************************************************
+Valtozo.cpp
+
+****************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a;
+  double  b;
+  cout << "a= " << a << endl;
+  cout << "b= " << b << endl;
+  return 0;
+}
